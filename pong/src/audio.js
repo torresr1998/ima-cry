2daa2da365b539b3e6bf08100dd414d2ca9db3a3
torresr1998 <121883527+torresr1998@users.noreply.github.com>
2023-01-14 08:24:39 -0600

    Update audio.js

--- pong/src/audio.js
+++ pong/src/audio.js
@@ -52,17 +52,17 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("phaserDown2.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("ocean-waves.mp3");
+export var ambientSound = new soundFile("254684__valentinsosnitskiy__darck-water (1).mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("Adventure_Meme.mp3");
+export var adventureMusic = new soundFile("254684__valentinsosnitskiy__darck-water (1).mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
+export var villageMusic = new soundFile("254684__valentinsosnitskiy__darck-water (1).mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
